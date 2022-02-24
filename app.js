400b0e0c9bbeee7344db876bf67faf687a34a9f6
vaishnavithakare2807 <vaishnavithaakare@gmail.com>
2022-02-24 09:48:30 +0530

    add

---
+++ app.js
@@ -0,0 +1,93 @@
+// // NaN
+// // Number has only one type in javascript. It will not differentiate between positive , negative, decimal number
+// var a = 0;
+// console.log(typeof 0);
+// var a = 4.2;
+// console.log(typeof a);
+// var a = -9;
+// console.log(typeof a);
+// var a = 0 / 0;
+// console.log(typeof a);
+// console.log(typeof NaN);
+// var a = NaN + 1;
+// var a = NaN - 2;
+// var a = NaN / 1;
+// var a = NaN * 2;
+// // It is valid in js to do mathematical operation with NaN. It will not show you any kind of error.
+// console.log(typeof NaN)
+
+
+// // Updating the value assign to var, let and const
+// var a = 10;
+// console.log(a)
+// var a = 20;
+// console.log(a)
+
+
+// let b = 100;
+// console.log(b);
+// b = 200;
+// console.log(b);
+// // let b = 500;
+// // console.log(b)
+// // Why after writing let again for same variable it is showing an error ?
+
+
+// const c = 300;
+// console.log(c);
+// // const c = 400;
+// // console.log(c)
+// // 1. We have to declare and assign the value at same time in case of const.
+// // 2. It will show you syantax error that c is already been declared.
+
+// const num=2;
+// if (num==2){
+//     console.log("The number is 2")
+// }
+
+const day = 1;
+switch (3) {
+    case 1:
+        console.log("Monday")
+        break;
+    case 2:
+        console.log("Thuesday")
+        break;
+    case 3:
+        console.log("Wednesday")
+        break;
+        
+       
+
+    default:
+        console.log("I dont know")
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Boolean
+
+
+// var a = true;
+// console.log(a);
+// a = 45122;
+// console.log(a)
